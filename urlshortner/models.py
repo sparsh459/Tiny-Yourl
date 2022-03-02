d2c669530fc675ad65cdfe86ed32ebbd21822b33
Sparsh Saxena <86906056+sparsh459@users.noreply.github.com>
2022-03-03 00:08:30 +0530

    Update models.py

--- urlshortner/models.py
+++ urlshortner/models.py
@@ -4,4 +4,4 @@ from django.db import models
 class url(models.Model):
     # link will take the url that we'll give to shorten
     link = models.CharField(max_length=10000)
-    uuid = models.CharField(max_length=50)
+    uuid = models.CharField(max_length=50)    
